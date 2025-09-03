efcb505c44a75a147d3e329f9d3bf4c65325c9ad
melabreu <melissa.abreu84@gmail.com>
2025-09-03 11:18:15 -0400

    setup complete

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+const express = require('express');
+const mongoose = require("mongoose"); // require package
+
+const app = express();
+
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
+require('dotenv').config();
+console.log(process.env);
